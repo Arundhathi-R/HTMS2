bb50b16cee3c2dacdc81cc9ee0adddac0cd166b1
gadha24 <gadhasabu24@gmail.com>
2024-05-12 20:51:02 +0530

    singnup,livetoken,appntbook,viewappointment

--- miniproj/src/App.js
+++ miniproj/src/App.js
@@ -5,6 +5,10 @@ import Login from './login/login';
 import PatientPortal from './patient portal/patient';
 import ConfirmationPage from './appmnt confirm/confirm';
 import DoctorSlotEditor from './edit slots/editslot';
+import Signup from './signup/signup';
+import LiveTokenDisplay from './livetoken/livetoken';
+import Viewdetails from './viewappointment/view';
+import Appntbook from './appntbook/appntbook';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
         <Route path="/patient portal/patient" element={<PatientPortal />} />
         <Route path="/appmnt confirm/confirm" element={<ConfirmationPage />} />
         <Route path="/edit slots/editslot" element={<DoctorSlotEditor />} />
+        <Route exact path="/signup/signup" element={<Signup/>} />
+        <Route path="/appntbook/appntbook" element={<Appntbook/>} />
+        <Route path="/livetoken/livetoken" element={<LiveTokenDisplay/>}/>
+        <Route path="/viewappointment/view" element={<Viewdetails/>}/>
       </Routes>
     </Router>
   );
