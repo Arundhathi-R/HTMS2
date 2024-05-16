02af631402ff305239df01b19fc05ea78bac7489
Aelan Anna Mathew <aelanannamathew@gmail.com>
2024-05-17 02:00:20 +0530

    updates

--- miniproj/src/login/login.js
+++ miniproj/src/login/login.js
@@ -1,121 +1,5 @@
 
-  /*return (
-    <div className="login-container">
-      <div className="login-form">
-        <h2>Login</h2>
-        <div className="input-container">
-          <label>Username</label>
-          <input 
-            type="email" 
-            placeholder="@gmail.com" 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required 
-          />
-        </div>
-        <div className="input-container">
-          <label>Password</label>
-          <input 
-            type="password" 
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required 
-          />
-        </div>
-        <div className="forgot-password">
-          <a href="/forgotpassword" onClick={handleForgotPasswordClick}>Forgot password?</a>
-        </div>
-        <div className="button-container">
-          <button onClick={handleSignIn}>Sign In</button>
-          <button onClick={handleSignUp}>Sign Up</button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default Login;*/
-
-/*import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './login.css';
-import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import {useState} from "react";
-const Login = () => {
-  const navigate = useNavigate();
-  
-  const handlesignupButtonClick = () => {
-    navigate('/signup');
-  };
-  //const[email,setEmail] = useState("")
-  //const[password,setPassword] = useState("")
-  //console.log(auth?.currentUser?.email)
  
-  const[email,setEmail] = useState("")
-  const[password,setPassword] = useState("")
-  console.log(auth?.currentUser?.email)
-  const handlesigninButtonClick = async () => {
-    try{await signInUserWithEmailAndPassword(auth, email, password)
-       navigate('/patient'); 
-      } catch(err){
-        console.error(err);
-       }
-       
-     };
-     const handleForgotPasswordClick = () => {
-      navigate('/forgotpassword');
-    };
-<<<<<<< HEAD
-  
-    
-  
-=======
-
-    const handlesigninButtonClick = async () => {
-        try {
-            // patient sign in with Firebase Authentication
-            await signInWithEmailAndPassword(auth, email, password);
-            navigate('/patient');
-        } catch (error) {
-            console.error('Error signing in:', error.message);
-            // error handling
-        }
-    };
-
-    const handleForgotPasswordClick = () => {
-        navigate('/forgotpassword');
-    };
-
->>>>>>> e25087edef4b55012ff604f0d287a97aca49f250
-    return (
-      <div className="login-container7">
-        <div className="login-form7">
-          <h2>Login</h2>
-          <div className="input-container7">
-            <label >Username</label>
-            <input type="text" placeholder="@gmail.com" onChange={(e) => setEmail(e.target.value)}/>
-          </div>
-          <div className="input-container7">
-            <label>Password</label>
-            <input type="password" onChange={(e) => setPassword(e.target.value)}/>
-          </div>
-          <div className="forgot-password7">
-            <a href="/forgotpassword" onClick={handleForgotPasswordClick}>Forgot password?</a>
-          </div>
-          <div className="button-container7">
-            <button onClick={handlesigninButtonClick}>Sign In</button>
-             <button onClick={handlesignupButtonClick}>Sign Up</button>
-             </div>
-      </div>
-    </div>
-  );
-}
-
-export default Login;*/
-
-
 
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -140,7 +24,7 @@ const Login = () => {
   const handleSignUp = async () => {
     try {
       await createUserWithEmailAndPassword(auth, email, password);
-      navigate('/patient');
+      navigate('/siginup');
     } catch (error) {
       console.error("Error signing up:", error);
     }
